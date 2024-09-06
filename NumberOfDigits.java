55664bbc4cb5678abbcfc7be04d4f8d8bee4a648
Arun-06 <90039797+KuttyArun0601@users.noreply.github.com>
2024-09-07 00:17:38 +0530

    count the number of digits

---
+++ NumberOfDigits.java
@@ -0,0 +1,13 @@
+package interview_prgs;
+
+public class NumberOfDigits {
+
+	public static void main(String[] args) {
+		
+		int input = 324765432;
+		
+		int count = String.valueOf(input).length();
+		
+		System.out.println(count);
+	}
+}
